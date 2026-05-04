04cd357d50429e5e7be79ecfcfc688cc5a584252
Alan Felipe Monteiro de Andrade <alanmmonteiro105@gmail.com>
2026-05-04 15:13:57 -0300

    chore(db): setup mikro-orm configuration with postgresql

---
+++ mikro-orm.config.ts
@@ -0,0 +1,20 @@
+// apps/jg-wallet-service/mikro-orm.config.ts
+import { Options, PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { WalletSchema } from './src/modules/wallet/adapters/out/WalletSchema';
+
+const config: Options = {
+  driver: PostgreSqlDriver,
+  dbName: 'igaming_wallet',
+  user: 'jungle_user',
+  password: 'jungle_password',
+  host: '127.0.0.1',
+  port: 5432,
+  entities: [WalletSchema],
+  debug: true, // Excelente para ver as queries geradas no terminal durante o desenvolvimento
+  migrations: {
+    path: './src/migrations',
+    emit: 'ts',
+  },
+};
+
+export default config;
